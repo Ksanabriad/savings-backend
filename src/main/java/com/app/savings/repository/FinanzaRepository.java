6b8a9dd1e0b9e47542445939816f3aa08d12dea3
ksanabriad <ksanabriad@gmail.com>
2026-01-30 12:13:21 +0100

    repository

--- src/main/java/com/app/savings/repository/FinanzaRepository.java
+++ src/main/java/com/app/savings/repository/FinanzaRepository.java
@@ -1,9 +1,17 @@
 package com.app.savings.repository;
 
 import com.app.savings.entities.Finanza;
+import com.app.savings.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface FinanzaRepository extends JpaRepository<Finanza, Long> {
     List<Finanza> findByUsuarioUsername(String username);
+
+    @Query("SELECT f FROM Finanza f WHERE f.usuario = :usuario AND YEAR(f.fecha) = :anio AND MONTH(f.fecha) = :mes")
+    List<Finanza> findByUsuarioAndMesAndAnio(@Param("usuario") Usuario usuario, @Param("mes") int mes,
+            @Param("anio") int anio);
 }
